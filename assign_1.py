95b3448b1dfdccf0a3ad89eb3f7d553821fb01a0
Ishan Vatsa <ishanvatsa65@gmail.com>
2025-11-23 17:55:09 +0530

    first assignment

---
+++ assign_1.py
@@ -0,0 +1,21 @@
+#Task 1
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+
+
+# Task 2
+
+x=input("Enter your first name :")
+y=input("Enter your last name :")
+z=x+" "+y
+print("Hello,",z,"!","Welcome to the Python program.")
